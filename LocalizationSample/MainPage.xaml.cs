5b4fb56524613d7a9e61f1b353be3e53381fb63d
Chris Koenig <chris.koenig@microsoft.com>
2012-05-16 18:55:18 -0500

    first commit

---
+++ LocalizationSample/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using LocalizationSample.Resources;
+using Microsoft.Phone.Controls;
+
+namespace LocalizationSample
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void RunSoundFileButton_Click(object sender, EventArgs e)
+        {
+            MyMediaElement.Play();
+        }
+    }
+}
